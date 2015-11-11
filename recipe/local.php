20bed492d6909ddb03d4fc8ae517b687f16fb7a5
Vladimir Pak <TheRatW@gmail.com>
2015-11-11 10:52:36 +0200

    check remote branch exists fix

--- recipe/local.php
+++ recipe/local.php
@@ -36,10 +36,11 @@ task('project-update:update_code', function () {
         $branch = run('cd {{release_path}} && git rev-parse --abbrev-ref HEAD')
             ->toString();
     }
-    $res = trim(run("git for-each-ref --format='%(upstream:short)' $(git symbolic-ref HEAD)")->toString());
+    $repository = get('repository');
+    $res = trim(run("git ls-remote $repository $(git symbolic-ref HEAD)")->toString());
     if ($res) {
         run("git pull origin $branch 2>&1");
     } else {
-        writeln("<comment>Found local git branch. Pulling skipped.</comment>");
+        writeln("<comment>Remote $branch not found</comment>");
     }
 })->desc('Updating code');
