f2c2af00acda188f003985659a5abd91915528b4
Vladimir Pak <v.pak@roboforex.com>
2017-03-01 19:18:09 +0000

    bug fix

--- src/ReleaseInfo.php
+++ src/ReleaseInfo.php
@@ -174,7 +174,7 @@ class ReleaseInfo
                  */
                 writeln(' * '.$issue->__toString());
             }
-        } elseif (get(self::PARAMETER_TASK_LIST)) {
+        } elseif (has(self::PARAMETER_TASK_LIST)) {
             writeln('Deployed:');
             foreach (get(self::PARAMETER_TASK_LIST) as $task) {
                 writeln(' * '.$task);
